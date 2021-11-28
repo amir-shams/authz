777ee54045a6ea67e63e2a9cafd43b825ce0a8e7
paladin <amir.shamss20@gmail.com>
2021-11-28 21:09:19 +0330

    create application structure

---
+++ authz/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+from authz.config import Config
+
+def create_app():
+	app = Flask(__name__)
+	app.config.from_object(Config)  # load configs from env variables .
+	return app
