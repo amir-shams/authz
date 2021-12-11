0d5bdffd144fb90b34c2bf7fb8a25df797e3ae23
paladin <amir.shamss20@gmail.com>
2021-12-11 23:15:00 +0330

    define now function

--- authz/util/__init__.py
+++ authz/util/__init__.py
@@ -1,1 +1,2 @@
 from authz.util.uuidgen import uuidgen
+from authz.util.datetime import now
---
+++ authz/util/datetime.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+def now():
+    """
+    Get current system datetime.
+    """
+    return datetime.now()
