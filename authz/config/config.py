bfd46b21003a342896c25660e241e6b01bef2d4c
Amir Shamsi <amir.shamss21@gmail.com>
2022-01-16 17:54:16 +0330

    adding user model

--- authz/config/config.py
+++ authz/config/config.py
@@ -11,7 +11,7 @@ class Config:
 
 	##########  database Configuration  ##########
 
-	SQLALCHEMY_DATABASE_URI = environ.get("SKOB_AUTHZ_DATABASE_URI", None)
+	SQLALCHEMY_DATABASE_URI = environ.get("SKOB_AUTHZ_DATABASE_URI", "mysql+pymysql://authzuser:authzpass@localhost:3306/authz")    #None
 
 	SQLALCHEMY_TRACK_MODIFICATIONS = TESTING
 
